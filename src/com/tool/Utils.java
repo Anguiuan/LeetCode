a90e007262fd87bec3d4b7bee95b5fba122bc585
zkyuan <2584278161@qq.com>
2023-02-24 19:43:19 +0800

    000

--- src/com/tool/Utils.java
+++ src/com/tool/Utils.java
@@ -1,18 +1,17 @@
 package com.tool;
 
-
 /**
- * @author: yuan
- * @date: 2023/2/23
- * @Description: 工具类
+ * @auther: zkyuan
+ * @date: 2023/2/22 22:19
+ * @description: 交换两数
  */
 public class Utils {
     /**
-     * @author: yuan
-     * @date: 2023/2/23 11:29
-     * @param: [arr, i, j]
-     * @return: int[]
-     * @descipton: 数组交换两数。讲数组arr的i下标和j下标的数互换
+     * @author: zkyuan
+     * @Description: 交换两数
+     * @Param: [arr, i, j]
+     * @Return: int[]
+     * @DateTime: 2023/2/22 22:17
      */
     public static int[] swap(int[] arr, int i, int j) {
         arr[i] = arr[i] ^ arr[j];
