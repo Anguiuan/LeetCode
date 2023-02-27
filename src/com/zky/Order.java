3524267affc06509a8d10b9934c271c7e7b52136
yuan <2584278161@qq.com>
2023-02-27 17:49:58 +0800

    排序算法--快速排序、希尔排序

--- src/com/zky/Order.java
+++ src/com/zky/Order.java
@@ -1,5 +1,6 @@
 package com.zky;
 
+import com.tool.ListNode;
 import com.tool.Utils;
 
 /**
@@ -9,17 +10,33 @@ import com.tool.Utils;
  */
 public class Order {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 2, 5, 9, 6, 3, 4, 7, 8, 2, 1, 5};
+        int[] arr = {3, 2, 1, 3, 2, 5, 9, 6, 3, 4, 7, 8, 2, 1, 5};
         System.out.print("冒泡排序结果：");
         int[] bubbling = bubblingOrder(arr);
         for (int num : bubbling) {
-            System.out.print(num);
+            System.out.print(num + " ");
         }
-        System.out.print("\n插入排序结果：");
+        System.out.print("\n直接插入结果：");
         int[] inse = insertOrder(arr);
         for (int num : inse) {
-            System.out.print(num);
+            System.out.print(num + " ");
         }
+        System.out.print("\n折半插入结果：");
+        int[] in = insertSort(arr);
+        for (int num : in) {
+            System.out.print(num + " ");
+        }
+        System.out.print("\n希尔排序结果：");
+        int[] shell = insertSort(arr);
+        for (int num : shell) {
+            System.out.print(num + " ");
+        }
+        System.out.print("\n快速排序结果：");
+        int[] quickSort = quickSort(arr);
+        for (int num : quickSort) {
+            System.out.print(num + " ");
+        }
+
     }
 
     /**
@@ -51,7 +68,7 @@ public class Order {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
-                    Utils.swap(arr, j, j - 1);
+                    Utils.swap(arr, j, j - 1);//交换两数
                 } else {
                     break;
                 }
@@ -60,4 +77,110 @@ public class Order {
         return arr;
     }
 
+    /**
+     * @author: yuan
+     * @date: 2023/2/27 10:30
+     * @param: [arr]
+     * @return: int[]
+     * @descipton: 折半插入。在进行每一趟寻找待插入位置的操作时，使用折半查找的方式
+     */
+    public static int[] insertSort(int[] arr) {
+        //外层循环控制循环次数
+        for (int i = 2; i < arr.length; i++) {
+            //临时变量保存待插入元素
+            int temp = arr[i];
+            //有序列表的左右指针
+            int left = 0, right = i - 1;
+            //内层循环寻找插入的位置
+            while ((right - left) >= 0) {
+                int mid = (left + right) / 2;
+                if (arr[mid] < temp) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            //在待插入位置，数组整体后移
+            for (int j = i; j > left; j--) {
+                arr[j] = arr[j - 1];
+            }
+            //放入元素
+            arr[left] = temp;
+        }
+        return arr;
+    }
+
+    /**
+     * @author: yuan
+     * @date: 2023/2/27 14:47
+     * @param: [arr]
+     * @return: int[]
+     * @descipton: 希尔排序，先让元素基本有序，再用直接插入排序
+     */
+    public static int[] shellSort(int[] arr) {
+        //数组长度
+        int n = arr.length;
+        //外层循环按相同增量的距离分组
+        for (int d = n / 2; d > 0; d = d / 2) {
+            //对分组进行直接插入排序
+            for (int i = d; i < n; i++) {
+                //i位置为待排序判断的元素，i之前为有序集合
+                if (arr[i] < arr[i - d]) {
+                    //临时中间变量temp、排序控制变量j
+                    int temp = arr[i], j = i - d;
+                    //在要插入的位置后的数组元素后移
+                    for (; j > 0 && temp < arr[j]; j -= d) {
+                        arr[j + d] = arr[j];
+                    }
+                    //插入元素
+                    arr[j + d] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * @author: yuan
+     * @date: 2023/2/27 16:41
+     * @param: [arr, low, high]
+     * @return: void
+     * @descipton: 快速排序，递归
+     */
+    public static void quickSort(int[] arr, int left, int right) {
+        //递归返回条件
+        if (left > right) {
+            return;
+        }
+        //pivot就是基准位
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+        while (i < j) {
+            //先看右边，依次往左递减
+            while (pivot <= arr[j] && i < j) {
+                j--;
+            }
+            //再看左边，依次往右递增
+            while (pivot >= arr[i] && i < j) {
+                i++;
+            }
+            //如果满足条件则交换
+            if (i < j) {
+                Utils.swap(arr, i, j);
+            }
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[left] = arr[i];
+        arr[i] = pivot;
+        //递归调用左半数组
+        quickSort(arr, left, j - 1);
+        //递归调用右半数组
+        quickSort(arr, j + 1, right);
+    }
+
+    public static int[] quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+        return arr;
+    }
 }
--- src/com/zky/Test.java
+++ src/com/zky/Test.java
@@ -2,16 +2,60 @@ package com.zky;
 
 public class Test {
     /**
-     *
      * @author: yuan
      * @date: 2023/1/4
      * Description:  测试算法
      */
     public static void main(String[] args) {
-        int[] nums1 = {1,2,6,7,9};
+        /*int[] nums1 = {1,2,6,7,9};
         int[] nums2 = {3,5,8,10};
         FindMedianSortedArrays findMedianSortedArrays = new FindMedianSortedArrays();
         double d = findMedianSortedArrays.findMedianSortedArrays(nums1,nums2);
-        System.out.println(d);
+        System.out.println(d);*/
+        int[] arr = {10, 7, 2, 4, 7, 62, 3, 4, 2, 1, 8, 9, 19};
+        quickSort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"  ");
+
+        }
     }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        int i, j, temp, t;
+        if (low > high) {
+            return;
+        }
+        i = low;
+        j = high;
+        //temp就是基准位
+        temp = arr[low];
+
+        while (i < j) {
+            //先看右边，依次往左递减
+            while (temp <= arr[j] && i < j) {
+                j--;
+            }
+            //再看左边，依次往右递增
+            while (temp >= arr[i] && i < j) {
+                i++;
+            }
+            //如果满足条件则交换
+            if (i < j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[low] = arr[i];
+        arr[i] = temp;
+        //递归调用左半数组
+        quickSort(arr, low, j - 1);
+        //递归调用右半数组
+        quickSort(arr, j + 1, high);
+    }
+
+
 }
+
