9d609ccc7c42601d829a4017668285fb3e5c46aa
yuan <2584278161@qq.com>
2023-02-28 17:15:54 +0800

    堆排序

--- src/com/zky/Order.java
+++ src/com/zky/Order.java
@@ -41,6 +41,11 @@ public class Order {
         for (int num : choose) {
             System.out.print(num + " ");
         }
+        System.out.print("\n堆排序结果：");
+        int[] heap = heapSort(arr);
+        for (int num : heap) {
+            System.out.print(num + " ");
+        }
 
     }
 
@@ -212,5 +217,61 @@ public class Order {
             }
         }
         return arr;
+    }
+
+    /**
+     * @author: yuan
+     * @date: 2023/2/28 11:43
+     * @param: [arr]
+     * @return: void
+     * @descipton: 堆排序
+     */
+    public static int[] heapSort(int[] arr) {
+        //从倒数第一个非叶子节点开始
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            //从第一天非叶子节点从下至上，从左至右调整结构
+            adjustHeap(arr, i, arr.length);
+        }
+        //将堆顶元素与末尾元素交换 将最大元素沉到数组末尾 + 重新调整堆结构
+        for (int i = arr.length - 1; i > 0; i--) {
+            //交换堆顶元素和末尾元素
+            Utils.swap(arr, 0, i);
+            //交换后的末尾元素忽略(j--) 不再参与堆结构的调整
+            //重新调整堆结构
+            adjustHeap(arr, 0, i);
+        }
+        return arr;
+    }
+
+    /**
+     * @author: yuan
+     * @date: 2023/2/28 11:42
+     * @param: [arr, index, length]
+     * @return: void
+     * @descipton: 调整为小根堆
+     */
+    public static void adjustHeap(int[] arr, int index, int length) {
+        //取出当前元素
+        int temp = arr[index];
+        //i节点是index节点的左子节点
+        for (int i = 2 * index + 1; i < length; i = 2 * i + 1) {
+            //表明左子节点小于右子节点
+            if (i + 1 < length && arr[i] < arr[i + 1]) {
+                //将指针移至较大节点
+                i++;
+            }
+            //如果子节点大于父节点
+            if (arr[i] > temp) {
+                //将较大值赋给当前节点
+                arr[index] = arr[i];
+                //指针移向子节点
+                index = i;
+            } else {
+                break;
+            }
+        }
+        //循环结束，已经将最大值放在了堆顶
+        //将temp值放到最终的位置
+        arr[index] = temp;
     }
 }
